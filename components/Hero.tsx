3df93c63ba902b5590c9835660b5868dbb5a9482
Cayman <93142107+caymanwjeffers@users.noreply.github.com>
2023-10-12 21:14:22 -0400

    features section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,30 +6,32 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="bg-white">
-      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
+    <div className="bg-gray-900">
+      <div className="relative isolate overflow-hidden bg-gradient-to-b from-black">
         <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
           <div className="px-6 lg:px-0 lg:pt-4">
-            <div className="mx-auto max-w-2xl">
+            <div className="mx-auto max-w-3xl lg:max-w-3xl">
+              {" "}
+              {/* Adjusted width for desktop */}
               <div className="max-w-lg">
-                <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
                   Pet Tracking Redefined.
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat aliqua.
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Keep your pet safe with Lootpaw. Our trackers are smaller,
+                  last longer, and are more accurate than the competition. With
+                  LTE, GPS, and Bluetooth, you'll never lose your pet again.
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   <a
                     href="#"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                   >
                     Download
                   </a>
                   <a
                     href="#"
-                    className="text-sm font-semibold leading-6 text-gray-900"
+                    className="text-sm font-semibold leading-6 text-white hover:text-gray-300"
                   >
                     Learn More <span aria-hidden="true">→</span>
                   </a>
@@ -37,27 +39,30 @@ export default function Hero() {
               </div>
             </div>
           </div>
-          <div className="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
-            <div
-              className="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36"
-              aria-hidden="true"
-            />
+          <div className="mt-20 sm:mt-24 md:mx-auto md:max-w-xl lg:mx-0 lg:mt-0 lg:w-screen">
             <div className="shadow-lg md:rounded-3xl">
               <div className="bg-yellow-500 [clip-path:inset(0)] md:[clip-path:inset(0_round_theme(borderRadius.3xl))]">
                 <div
-                  className="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36"
+                  className="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-800 opacity-20 ring-1 ring-inset ring-gray-700 md:ml-20 lg:ml-36"
                   aria-hidden="true"
                 />
                 <div
-                  className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 md:rounded-3xl"
+                  className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-gray-700/50 md:rounded-3xl"
                   aria-hidden="true"
                 />
-                <Image src="/cat.png" alt="Cat" width={500} height={500} />
+                {/* Adjusted image size for desktop */}
+                <Image
+                  src="/cat.png"
+                  alt="Cat"
+                  width={600}
+                  height={600}
+                  className="lg:w-400 lg:h-400"
+                />
               </div>
             </div>
           </div>
         </div>
-        <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+        <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-gray-900 sm:h-32" />
       </div>
     </div>
   );
