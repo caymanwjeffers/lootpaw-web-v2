6724b54d45754d002c0ac2c775c931aa33ffb2bb
Cayman <93142107+caymanwjeffers@users.noreply.github.com>
2023-10-12 20:22:15 -0400

    wip hero

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="absolute flex w-full items-center justify-between p-4 bg-black z-50">
+      <Image src="/textLogo.png" alt="Lootpaw Logo" width={100} height={20} />
+      <Link href="#">
+        <button className="text-xs font-bold px-4 py-2 rounded-sm text-black bg-yellow-500">
+          Coming Soon
+        </button>
+      </Link>
+    </nav>
+  );
+}
